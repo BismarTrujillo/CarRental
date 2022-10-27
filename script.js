e96aa12589571f095ef8f05482a5059c5b2d321e
Bismar Trujillo <bismar20@hotmail.com>
2022-10-27 14:27:50 -0400

    carrental completed

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function init() {
 }
 function calculateBtnOnClick() {
     const numberOfDaysField = Number(document.getElementById("numberOfDaysField").value);
-    
+
     let extraPerDay = 0;
     let subTotal = numberOfDaysField * 29.99;
-    
+
     const gps = document.getElementById("gps").checked;
     const tollTag = document.getElementById("tollTag").checked;
     const roadSide = document.getElementById("roadSide").checked;
     const yes = document.getElementById("yes").checked;
-   
-    
-   
+
+
+
     if (tollTag) {
         extraPerDay += 3.95 * numberOfDaysField;
     }
@@ -30,17 +30,23 @@ function calculateBtnOnClick() {
     if (roadSide) {
         extraPerDay += 2.95 * numberOfDaysField;
     }
-   
+
     let total = subTotal + extraPerDay;
-    
-   
+
+
     if (yes) {
         total += subTotal * 0.3;
     }
-    
-    const rentalPrice = document.getElementById("rentalPrice");
-    rentalPrice.value = Math.round(total*100)/100;
-   
+
+    const rentalPriceField = document.getElementById("rentalPrice");
+    rentalPriceField.innerHTML = subTotal;
+    const optionsField = document.getElementById("options");
+    optionsField.innerHTML = Math.round(extraPerDay * 100) / 100;
+    const ageSurchargeField = document.getElementById("ageSurcharge");
+    ageSurchargeField.innerHTML = Math.round(subTotal * 30) / 100;
+    const totalField = document.getElementById("total");
+    totalField.innerHTML = Math.round(total * 100) / 100;
+
 
 
 }
